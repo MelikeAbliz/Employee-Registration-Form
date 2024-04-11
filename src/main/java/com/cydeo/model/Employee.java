9ecc86e3a7110e4ba878734c13f3421319ad95a2
MelikeAbliz <medine20171121@gmail.com>
2024-04-12 00:52:35 +0300

    completed employee form

--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -3,6 +3,8 @@ package com.cydeo.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.time.LocalDate;
 
 @Data
@@ -11,6 +13,8 @@ import java.time.LocalDate;
 public class Employee {
     private String firstName;
     private String lastName;
+//thymeleaf accepts yyyy-MM-dd format
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDay;
     private String email;
     private String password;
